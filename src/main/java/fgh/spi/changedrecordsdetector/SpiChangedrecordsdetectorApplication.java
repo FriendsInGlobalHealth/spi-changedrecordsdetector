6d090319a141b26ca98595a27415f50c657ee706
JPboane <jpboane@gmail.com>
2021-09-20 18:47:34 +0200

    Saving the job 2

--- src/main/java/fgh/spi/changedrecordsdetector/SpiChangedrecordsdetectorApplication.java
+++ src/main/java/fgh/spi/changedrecordsdetector/SpiChangedrecordsdetectorApplication.java
@@ -1,13 +1,8 @@
 package fgh.spi.changedrecordsdetector;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SpiChangedrecordsdetectorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpiChangedrecordsdetectorApplication.class, args);
 	}
 
 }
